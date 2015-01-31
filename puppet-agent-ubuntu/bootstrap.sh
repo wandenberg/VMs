ac7aba7bbfb2ad433183f57181d908866e883ad5
Wandenberg <wandenberg@gmail.com>
2015-01-31 18:09:39 -0200

    check if puppetlabs-release-trusty.deb is already downloaded

--- puppet-agent-ubuntu/bootstrap.sh
+++ puppet-agent-ubuntu/bootstrap.sh
@@ -6,7 +6,7 @@ sed -i '/puppet-master/ d' /etc/hosts
 sed -i '/puppet-agent/ d' /etc/hosts
 [[ `grep "192.168.33.11" /etc/hosts | wc -l` -eq 0 ]] && echo -e "192.168.33.11\tpuppet-agent.local.vms.com puppet-agent" >> /etc/hosts
 
-wget https://apt.puppetlabs.com/puppetlabs-release-trusty.deb
+[[ ! -f puppetlabs-release-trusty.deb ]] && wget https://apt.puppetlabs.com/puppetlabs-release-trusty.deb
 dpkg -i puppetlabs-release-trusty.deb
 apt-get update
 
@@ -18,3 +18,5 @@ sed -i 's/^START=no/START=yes/' /etc/default/puppet
 puppet resource service puppet ensure=running enable=true
 
 puppet agent --test
+
+exit 0
